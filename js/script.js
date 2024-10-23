2639e9ff6214844df60104027583afa26c391d56
Jawad <jawadnasiri2222@gmail.com>
2024-10-23 16:13:58 +0200

    Age calculator

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let userInput = document.querySelector('input[type="date"]');
+let btn = document.querySelector('#btn');
+
+
+userInput.max = new Date().toISOString().split('T')[0];
+
+btn.addEventListener('click', () => {
+
+    let birthDate = new Date(userInput.value);
+
+
+    let dayBirthday = birthDate.getDate();
+    let monthBirthday = birthDate.getMonth() + 1;
+    let yearBirthday = birthDate.getFullYear();
+
+
+    
+    let currentDate = new Date();
+    let currentDay = currentDate.getDate();
+    let currentMonth = currentDate.getMonth() + 1;
+    let currentYear = currentDate.getFullYear();
+
+    let userDayBirth, userMonthBirth, userYearBirth;
+
+    userYearBirth = currentYear - yearBirthday;
+
+    if (currentMonth >= monthBirthday) {
+        userMonthBirth = currentMonth - monthBirthday;
+    } else {
+        userMonthBirth = 12 - (monthBirthday - currentMonth);
+        userYearBirth--;
+    }
+
+    if (currentDay >= dayBirthday) {
+        userDayBirth = currentDay - dayBirthday;
+    } else {
+        let daysInPreviousMonth = new Date(currentYear, currentMonth - 1, 0).getDate();
+        userDayBirth = daysInPreviousMonth - (dayBirthday - currentDay);
+        userMonthBirth--; 
+    }
+
+    if (userMonthBirth < 0) {
+        userMonthBirth += 12;
+        userYearBirth--; 
+    }
+
+    console.log(`Age: ${userYearBirth} years, ${userMonthBirth} months, and ${userDayBirth} days.`);
+});
+
+
