b39f3bc73c8400d57908ca2942ea92761b2d5432
Jawad <jawadnasiri2222@gmail.com>
2024-10-24 09:59:25 +0200

    Age calculator updated

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-let userInput = document.querySelector('input[type="date"]');
-let btn = document.querySelector('#btn');
 
+let userInput = document.querySelector('input[type="date"]'); 
+let btn = document.querySelector('#btn');
 
 userInput.max = new Date().toISOString().split('T')[0];
 
 btn.addEventListener('click', () => {
+    let existingResult = document.querySelector('.result');
+    if (existingResult) {
+        existingResult.remove();
+    }
 
     let birthDate = new Date(userInput.value);
-
 
     let dayBirthday = birthDate.getDate();
     let monthBirthday = birthDate.getMonth() + 1;
     let yearBirthday = birthDate.getFullYear();
 
-
-    
     let currentDate = new Date();
     let currentDay = currentDate.getDate();
     let currentMonth = currentDate.getMonth() + 1;
@@ -36,7 +37,7 @@ btn.addEventListener('click', () => {
     } else {
         let daysInPreviousMonth = new Date(currentYear, currentMonth - 1, 0).getDate();
         userDayBirth = daysInPreviousMonth - (dayBirthday - currentDay);
-        userMonthBirth--; 
+        userMonthBirth--;
     }
 
     if (userMonthBirth < 0) {
@@ -44,7 +45,13 @@ btn.addEventListener('click', () => {
         userYearBirth--; 
     }
 
-    console.log(`Age: ${userYearBirth} years, ${userMonthBirth} months, and ${userDayBirth} days.`);
+    let resElement = document.createElement('div');
+    resElement.className = 'result';
+
+    let pElement = document.createElement('p');
+    
+    resElement.appendChild(pElement);
+    pElement.textContent = `Age: ${userYearBirth} years, ${userMonthBirth} months, and ${userDayBirth} days.`;
+
+    document.querySelector('.calculator').appendChild(resElement);
 });
-
-
